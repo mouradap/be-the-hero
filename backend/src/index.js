c248631a97e062a9bb84f4c8cad8f7d76c1db451
Denis Moura <drmouradap@gmail.com>
2020-03-25 16:57:11 -0300

    Adicionando o Backend da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+/* Rota / Recurso
+*/
+
+/* Métodos HTTP
+    GET: Buscar uma informação no backend
+    POST: Criar informação no backend
+    PUT: Alterar informação no backend
+    DELETE: Deleta informação no backend
+
+*/
+
+/* Tipos de Parâmetros
+
+    Query: Parâmetros nomeados na rota após o símbolo de ? (Filtros, paginação)
+    Route Params: Parâmetros utilizados para identificar recursos.
+    Request Body: Corpo da requisição, para criar ou alterar recursos.
+
+
+*/
+
+/* Bancos de dados
+
+    SQL:
+        MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server;
+    No SQL:
+        MongoDB, CouchDB, etc
+
+*/
+
+/* 
+    DRIVER: SELECT * FROM users
+
+    Query Builder: JavaScript ( table('users').select('*').where() )
+*/
+
+app.use(routes);
+
+app.listen(3333);
