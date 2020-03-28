f26af770a37c946f4b37a2ed5eb3e56d80e951d7
Denis Moura <drmouradap@gmail.com>
2020-03-28 14:01:59 -0300

    Adicionando testes e validações

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const cors = require('cors');
 
+const { errors } = require('celebrate');
+
 const routes = require('./routes');
 
 const app = express();
@@ -46,5 +48,6 @@ app.use(express.json());
 */
 
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
